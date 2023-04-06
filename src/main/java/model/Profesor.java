87c31a0f38ad41450c3bab280283417cbcccb65a
JS <javier_sinuka@mi.unc.edu.ar>
2023-04-06 18:35:00 -0300

    Create Alumno and Profesor class

--- src/main/java/model/Profesor.java
+++ src/main/java/model/Profesor.java
@@ -1,4 +1,20 @@
 package main.java.model;
 
 public class Profesor {
+    private String nombre_profesor;
+
+    public Profesor(String nombre_profesor) {
+        this.nombre_profesor = nombre_profesor;
+    }
+
+    public Profesor() {
+    }
+
+    public String getNombre_profesor() {
+        return nombre_profesor;
+    }
+
+    public void setNombre_profesor(String nombre_profesor) {
+        this.nombre_profesor = nombre_profesor;
+    }
 }
