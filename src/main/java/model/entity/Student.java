d013ddc60489bf13c1c2c9a07c12d2830e63d7a2
JS <javier_sinuka@mi.unc.edu.ar>
2023-05-10 14:39:16 -0300

    Creacion de metodos en Student, facilitando el manejo desde otras clases

--- src/main/java/model/entity/Student.java
+++ src/main/java/model/entity/Student.java
@@ -5,17 +5,16 @@ import main.java.model.objects.IDogeCoin;
 public class Student {
     private String nameStudent;
     private int lifeBar, coffeBar, walking, walkingB, credits;
+    private int dogeCoin;
 
-    IDogeCoin iDogueCoins[];
-
-    public Student(String nameStudent, int lifeBar, int coffeBar, int walking, int amountOfDogueCoin) {
+    public Student(String nameStudent, int amountLifeBar, int amountCoffeBar, int amountToWalk, int amountOfDogueCoin) {
         this.nameStudent = nameStudent;
-        this.lifeBar = lifeBar;
-        this.coffeBar = coffeBar;
-        this.walking = walking;
-        this.walkingB = walking;
+        this.lifeBar = amountLifeBar;
+        this.coffeBar = amountCoffeBar;
+        this.walking = amountToWalk;
+        this.walkingB = amountToWalk;
         this.credits = 0;
-        this.iDogueCoins = new IDogeCoin[amountOfDogueCoin];
+        this.dogeCoin = amountOfDogueCoin;
     }
 
     public Student() {
@@ -23,15 +22,19 @@ public class Student {
 
     //*************************** GETTERS *********************************
     public String getNameStudent() {
-        return nameStudent;
+        return this.nameStudent;
     }
-
     public int getLifeBar() {
-        return lifeBar;
+        return this.lifeBar;
     }
-
-    public int getAmountDogueCoinInWallet(){
-        return iDogueCoins.length;
+    public int getCoffeBar(){
+        return this.coffeBar;
+    }
+    public int getDogueCoinInWallet(){
+        return this.dogeCoin;
+    }
+    public int getCredits(){
+        return this.credits;
     }
 
     //*************************** SETTERS *********************************
@@ -41,37 +44,51 @@ public class Student {
     public void increseLifeBar(int lifeBar){
         this.lifeBar += lifeBar;
     }
-
-    /*
-    Decrementa uno de vida.
-     */
+    /**
+     Decrementa uno de vida.
+     **/
     public void decreaseLifeBar(){
         lifeBar--;
     }
-
-    /*
-    Decrementa un valor especifico de vida
-    */
+    /**
+     Decrementa un valor especifico de vida
+     **/
     public void decreaseLifeBar(int valueDerement){
         this.lifeBar -= valueDerement;
     }
+    public void addDogeCoin(int dogeCoin){
+        this.dogeCoin += dogeCoin;
+    }
+    public void decreaseDogeCoin(int amountDecrease){this.dogeCoin -= amountDecrease;}
+    public void setDogeCoin(int dogeCoin){this.dogeCoin = dogeCoin;}
+    /**
+     Decrementa uno de cafe.
+     **/
+    public void decreaseCafein(){
+        coffeBar--;
+    }
+    /**
+     Decrementa un valor especifico de cafe
+     **/
+    public void incrementCafein(int i){
+        coffeBar+= i;
+    }
+    public void addCredits(int credits){
+        this.credits += credits;
+    }
+    public void setCredits(int credits){this.credits = credits;}
 
-    /*
+    //*************************** METODOS PROPIOS *********************************
+
+    /**
     Corrobora los pasos hechos y va decrementando a medida que transciende entre paneles.
     Cuando llega a 0, se le resta uno de cafeina.
-     */
+     **/
     public void modifedWalkCafein(){
-
         walking--;
         if (walking == 0){
             decreaseCafein();
             walking = walkingB;
         }
-    }
-    public void decreaseCafein(){
-        coffeBar--;
-    }
-    public void incrementCafein(int i){
-        coffeBar+= i;
     }
 }
