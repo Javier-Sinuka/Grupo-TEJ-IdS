a0732878b19e23208f18bb7d48773aec32fc3c9b
JS <javier_sinuka@mi.unc.edu.ar>
2023-05-10 15:09:04 -0300

    no se sube mas la clase main

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -7,8 +7,11 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        GameManager gm=new GameManager();
-        gm.getWindow().revalidate();
-        gm.getWindow().repaint();
+//        GameManager gm=new GameManager();
+//        gm.getWindow().revalidate();
+//        gm.getWindow().repaint();
+//        Hallway hallway = new Hallway();
+        Random random = new Random();
+        int randomNumber = random.nextInt((1 - 0) + 1) +0;
     }
 }
