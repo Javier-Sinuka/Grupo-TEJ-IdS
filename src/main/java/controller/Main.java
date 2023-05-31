f351c38021bbdd3c6f60f93fb4781089b831521a
Javier Sinuka <javier_sinuka@mi.unc.edu.ar>
2023-05-31 14:33:00 -0300

    cambios antes del merge

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -8,8 +8,7 @@ import main.java.model.objects.Consumable;
 import main.java.model.objects.Item;
 import main.java.model.objects.Usable;
 
-import java.io.IOException;
-import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
