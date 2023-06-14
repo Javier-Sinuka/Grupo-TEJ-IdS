7d674da3e2961c5cb1dce769ea7f015bfa208042
Javier Sinuka <javier_sinuka@mi.unc.edu.ar>
2023-06-14 20:19:18 -0300

    Arreglado respuesta correcta

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,12 +1,19 @@
 package controller;
 
+import model.entity.DBExams;
+
 import java.io.IOException;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        GameManager gm=new GameManager();
-        gm.getWindow().revalidate();
-        gm.getWindow().repaint();
+//        GameManager gm=new GameManager();
+//        gm.getWindow().revalidate();
+//        gm.getWindow().repaint();
+        DBExams dbExams = new DBExams();
+        for (String s : dbExams.getAnswers("Fisica I","¿Es necesario tener pasion por el rock para entender a Bethoven?")){
+            System.out.println(s);
+        }
+
     }
 }
